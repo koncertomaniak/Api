58d7ab6712ed5827c62d1c7f63f131be036d20ca
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2023-06-15 13:51:20 +0200

    Devops/test workflow (#1)

--- scripts/migration.sh
+++ scripts/migration.sh
@@ -4,7 +4,8 @@ dotnet ef database update -p ./src/Modules/Event/Koncertomaniak.Api.Module.Event
 dotnet ef database update -p ./src/Modules/Ticket/Koncertomaniak.Api.Module.Ticket.Infrastructure
 dotnet ef database update -p ./src/Modules/Auth/Koncertomaniak.Api.Module.Auth.Infrastructure
 psql --version
-psql -h postgres -p 5432 -U test -d koncertomaniak -f data/Events.sql < test 
-psql -h postgres -p 5432 -U test -d koncertomaniak -f data/TicketProviders.sql < test
-psql -h postgres -p 5432 -U test -d koncertomaniak -f data/EventTickets.sql < test 
-psql -h postgres -p 5432 -U test -d koncertomaniak -f data/ApiClients.sql < test 
+export PGPASSWORD="test"
+psql -h localhost -p 5432 -U test -d koncertomaniak -f data/Events.sql
+psql -h localhost -p 5432 -U test -d koncertomaniak -f data/TicketProviders.sql
+psql -h localhost -p 5432 -U test -d koncertomaniak -f data/EventTickets.sql
+psql -h localhost -p 5432 -U test -d koncertomaniak -f data/ApiClients.sql
